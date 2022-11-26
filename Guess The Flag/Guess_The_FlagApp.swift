f7697a20e243c48ff1ab96ee541f0af6008364fd
khubak <103487011+khubak@users.noreply.github.com>
2022-11-26 18:09:11 +0100

    guess the flag

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Karlo Hubak on 26.11.2022..
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
